a115e5a09bccfa19be0e788944ad6bf8763dac8b
Linh Van Hieu <linh.hieu@piscada.com>
2023-12-20 10:14:52 +0700

    remove files

--- packages/theme/src/index.tsx
+++ packages/theme/src/index.tsx
@@ -2,4 +2,3 @@ export { default as ThemeComponent } from "./theme/ThemeComponent";
 export { SettingsConsumer, SettingsProvider } from "./context/settingsContext";
 export { useSettings } from "./hooks/useSettings";
 export { styled, useTheme, Theme } from "@mui/material/styles";
-export { default as ThemeToggleButton } from "./components/theme-toggle-button";
